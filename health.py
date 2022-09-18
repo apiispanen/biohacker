dfb761a48fb7c1395baadc5aec2b2e60afc4360c
Andrew Piispanen <apiispanen1@babson.edu>
2022-09-18 16:37:48 -0400

    Health Py reads Resources/health_export

--- health.py
+++ health.py
@@ -6,9 +6,13 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 from lxml import etree
 from lxml import etree
+import datetime as dt
 parser = etree.XMLParser(remove_comments=True)
 
+# ****** DATA TO READ BELOW *******
 health_xml = 'Resources/health_export-916.xml'
+# *********
+
 tree = ET.parse(health_xml, parser=parser)
 root = tree.getroot()
 records = root.findall("Record")
@@ -17,19 +21,33 @@ types_of_interest = ['HKQuantityTypeIdentifierStepCount','HKQuantityTypeIdentifi
 
 df_data = [] 
 
+
+print(dt.datetime.strptime('2022-09-13 21:52:05 -0400'[:-6], "%Y-%m-%d %H:%M:%S"))
+
+
 for x in root:
     try:    
         if x.attrib['type'] in types_of_interest:
-            df_data.append([x.attrib['type'],x.attrib['startDate'], x.attrib['endDate'],x.attrib['unit'], x.attrib['value']])
+            df_data.append([
+                x.attrib['type'],
+                dt.datetime.strptime(x.attrib['startDate'][:-6], "%Y-%m-%d %H:%M:%S"), 
+                dt.datetime.strptime(x.attrib['endDate'][:-6], "%Y-%m-%d %H:%M:%S"),
+                x.attrib['unit'], 
+                float(x.attrib['value'])
+                ])
+
     except Exception as e:
         try:
-            df_data.append([x.attrib['type'],x.attrib['startDate'], x.attrib['endDate']]) 
+            df_data.append([x.attrib['type'],dt.datetime.strptime(x.attrib['startDate'][:-6], "%Y-%m-%d %H:%M:%S"), dt.datetime.strptime(x.attrib['endDate'][:-6], "%Y-%m-%d %H:%M:%S")]) 
         except:
             print("*****FAILED TO UPLOAD****", x.attrib)
             print(str(e))
 
 
 health_df = pd.DataFrame(df_data, columns=['Type','startDate', 'endDate','unit', 'Value'])
-health_df.to_csv('Resources/latest_health_csv.csv')
-# print(health_df)
+
+
+# EXPORT IF DESIRED
+# health_df.to_csv('Resources/latest_health_csv.csv')
+
 
--- stream.py
+++ stream.py
@@ -1,3 +1,4 @@
+from turtle import fillcolor
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -6,7 +7,8 @@ from datetime import datetime as dt
 # from plot import plt
 import plotly.express as px
 import plotly.graph_objects as go
-
+from health import health_df
+from datetime import datetime as dt
 
 st.title("Biohackerz")
 
@@ -20,6 +22,28 @@ st.subheader('Tracking Mood over time')
 fig = px.scatter(data, x="timestamp", y="mood", text="mood", title='Drew has a bad mood', trendline='ols', trendline_color_override='green')
 fig.data[0].update(mode='markers+lines', fill='toself')
 fig.update_traces(textposition='top left')
+
 st.plotly_chart(fig, use_container_width=True)
 
-st.write("testing")
+
+
+# NEW CHART FOR HEALTH
+
+steps_health_df = health_df.loc[(health_df['Type'] == 'HKQuantityTypeIdentifierStepCount' )&
+    (health_df['endDate'] > dt(2022, 9, 8))
+    ].set_index('endDate').groupby(pd.Grouper(freq='D')).sum()
+# st.write(steps_health_df)
+
+fig = px.scatter(steps_health_df, x=steps_health_df.index, y="Value", title='Health Data Test: Total Steps')
+fig.data[0].update(mode='markers', fill='toself')
+fig.update_traces(marker=dict(size=12,
+                              line=dict(width=2,
+                                        color='DarkSlateGrey')),
+                  selector=dict(mode='markers'))
+
+fig.update_traces(textposition='top left')
+
+st.plotly_chart(fig, use_container_width=True)
+
+
+# st.write("testing")
